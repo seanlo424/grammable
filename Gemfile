c948e64c32b2e8d3c41cd5b6cf219de66812d359
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-09-21 04:39:02 +0000

    Added FactoryBot gem

--- Gemfile
+++ Gemfile
@@ -65,12 +65,14 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :development, :test do
   gem 'rspec-rails', '~> 3.5'
+  gem "factory_bot_rails"
 end
 
 gem 'popper_js', '~> 1.11.1'
 gem 'bootstrap', '4.0.0.alpha6'
 gem 'simple_form'
 gem 'devise'
+
 
 source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -12,16 +12,30 @@ RSpec.describe GramsController, type: :controller do
 	describe "grams#new action" do
 
     it "should require users to be logged in" do
-      post :create, params: { gram: {message: "Hello" } }
+      get :new
       expect(response).to redirect_to new_user_session_path
     end
 
+    it "should successfully show the new form" do
+      user = FactoryBot.create(:user)
+      sign_in user
+
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+
+  describe "grams#create action" do
+
+    it "should require users to be logged in" do
+      post :create, params: { gram: { message: 'Hello!' } }
+      expect(response).to redirect_to new_user_session_path
+    end
+
+
     it "should successfully create a new gram in our database" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-        )
+      user = FactoryBot.create(:user)
       sign_in user
 
       post :create, params: { gram: { message: 'Hello!' } }
@@ -33,17 +47,13 @@ RSpec.describe GramsController, type: :controller do
     end
 
     it "should properly deal with validation errors" do
-      user = User.create(
-        email:                 'fakeuser@gmail.com',
-        password:              'secretPassword',
-        password_confirmation: 'secretPassword'
-        )
+      user = FactoryBot.create(:user)
       sign_in user
 
       gram_count = Gram.count
-      post :create, params: {gram: { message: ''}}
+      post :create, params: { gram: { message: ''} }
       expect(response).to have_http_status(:unprocessable_entity)
-      expect(Gram.count).to eq Gram.count
+      expect(gram_count).to eq Gram.count
 	  end
 
   end

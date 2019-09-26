38b4f99babb331933935cc75329aa9bb929993c0
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2019-09-26 03:56:36 +0000

    Styled the App

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_23_211745) do
+ActiveRecord::Schema.define(version: 2019_09_24_034500) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "message"
+    t.integer "user_id"
+    t.integer "gram_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "grams", force: :cascade do |t|
     t.text "message"
